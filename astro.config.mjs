b9fdb5f747a30c7f29098b317456caf91764939b
apollo <abir.deol2006@gmail.com>
2026-07-30 07:16:17 -0700

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://apollo-2006.github.io',
-    base: '/personal_portfolio',
+    site: 'abirdeol.tech',
 });
